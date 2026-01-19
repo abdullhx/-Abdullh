5f0049d06f9f331f21a06672d894e73686137940
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-01-20 00:27:04 +0300

    Update WallShaigavztba.lua

--- WallShaigavztba.lua
+++ WallShaigavztba.lua
@@ -1,30 +1,25 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-‎-- محاولة جلب الريموت بطريقة آمنة
-local ok, REGunRemote = pcall(function()
-    return ReplicatedStorage:FindFirstChild("Modules")
-       and ReplicatedStorage.Modules:FindFirstChild("Packages")
-       and ReplicatedStorage.Modules.Packages._Index["sleitnick_net@0.2.0"].net["RE/GunRemote"]
-end)
+local REGunRemote =
+    ReplicatedStorage
+    .Modules
+    .Packages
+    ._Index["sleitnick_net@0.2.0"]
+    .net["RE/GunRemote"]
 
-if not ok or not REGunRemote then
-    warn("REGunRemote not found")
-    return
-end
-
-‎-- حفظ namecall الأصلي
 local oldNamecall
 oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
-    local method = getnamecallmethod()
     local args = {...}
+    local method = getnamecallmethod()
 
-‎    -- اعتراض إطلاق النار فقط
-    if self == REGunRemote and method == "FireServer" then
-        if args[1] == "Shoot" then
-            -- Vector NaN لكسر حسابات السيرفر
-            args[3] = Vector3.new(0/0, 0/0, 0/0)
-            return oldNamecall(self, unpack(args))
-        end
+    if self == REGunRemote
+        and method == "FireServer"
+        and args[1] == "Shoot"
+    then
+‎        -- تعديل الـ argument الثالث فقط
+        args[3] = Vector3.new(0/0, 0/0, 0/0)
+
+        return oldNamecall(self, unpack(args))
     end
 
     return oldNamecall(self, ...)
